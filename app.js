27e3cb52f3390dc28bde1bdbdaced32c9c7b366e
Stalloyde <stalloyde93@gmail.com>
2024-05-07 22:16:51 +0800

    Set up CORS

--- app.js
+++ app.js
@@ -2,17 +2,30 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
+const corsOptions = {
+  origin: [
+    //add deployed url here when in production
+    'http://localhost:5173',
+    'http://localhost:3000',
+  ],
+  optionsSuccessStatus: 200,
+};
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors(corsOptions));
+app.options('*', cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
