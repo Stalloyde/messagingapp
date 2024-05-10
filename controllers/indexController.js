f6b354c03de42d6a71300d44818adf8041ae1b4a
Stalloyde <stalloyde93@gmail.com>
2024-05-10 14:02:54 +0800

    Implement local sign up

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,75 @@
+const User = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const expressAsyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+
 exports.signupGET = async (req, res, next) => {
   res.send('GET - Sign Up page');
 };
 
-exports.signupPOST = async (req, res, next) => {
-  res.send('POST - Sign Up page');
-};
+exports.signupPOST = [
+  body('username').notEmpty().trim().escape().withMessage('Username required'),
+  body('password').notEmpty().trim().escape().withMessage('Password required'),
+  body('confirmPassword')
+    .notEmpty()
+    .trim()
+    .custom((value, { req }) => value === req.body.password)
+    .escape()
+    .withMessage('Passwords do not match'),
+
+  expressAsyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const jsonErrorResponses = {
+      usernameError: null,
+      passwordError: null,
+      confirmPasswordError: null,
+    };
+
+    if (!errors.isEmpty()) {
+      const errorsArray = errors.array();
+
+      errorsArray.forEach((error) => {
+        if (error.path === 'username') {
+          jsonErrorResponses.usernameError = `*${error.msg}`;
+        } else if (error.path === 'password') {
+          jsonErrorResponses.passwordError = `*${error.msg}`;
+        } else {
+          jsonErrorResponses.confirmPasswordError = `*${error.msg}`;
+        }
+      });
+      return res.json(jsonErrorResponses);
+    }
+
+    let newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      status: null,
+      contacts: null,
+      profilePic: null,
+      messages: null,
+      contactsRequests: null,
+    });
+
+    try {
+      const { username } = newUser;
+      const checkDuplicate = await User.findOne({ username });
+
+      if (checkDuplicate) {
+        return res.json('*Username has been taken. Try another.');
+      }
+
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      newUser.password = hashedPassword;
+      await newUser.save();
+      return res.json('Sign up successful!');
+    } catch (err) {
+      return next(err);
+    }
+  }),
+];
 
 exports.loginGET = async (req, res, next) => {
   res.send('GET - Login page');
