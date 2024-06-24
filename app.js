a15212cd8477f7cc86bb0efe28dd7356cb8a50a5
Stalloyde <stalloyde93@gmail.com>
2024-06-24 17:48:17 +0800

    Add client production url to socket cors origin

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.options('*', cors());
 const io = new Server(server, {
   cors: {
     origin: [
-      //add deployed url here when in production
+      'https://messagingapp-client.vercel.app/',
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:3000',
