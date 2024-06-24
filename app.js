43598150405f55984315fd1f0e34ece3611962b2
Stalloyde <stalloyde93@gmail.com>
2024-06-24 13:58:05 +0800

    Add production dependencies

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
+const helmet = require('helmet');
 const passport = require('passport');
 const session = require('express-session');
 const cors = require('cors');
@@ -15,6 +17,12 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 const server = http.createServer(app);
+
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 300,
+});
 
 const corsOptions = {
   origin: [
@@ -30,6 +38,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
